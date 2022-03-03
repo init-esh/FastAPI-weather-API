9157ccd2ac088be28149a0c9d70f5eeb32fdbc36
Nitesh <niteshsoniofficial@gmail.com>
2022-03-03 16:06:16 +0530

    new venv and requirements.txt

--- test_main.py
+++ test_main.py
@@ -1,8 +1,10 @@
 from services import openweather_service as ows
 from models.validation_error import ValidationError
-
+import pytest
 def test_validate_units():
     assert ows.validate_units("Delhi", "DL", "", "metric") == ("delhi", "dl", "us", "metric")
     assert ows.validate_units("Delhi", "DL", "IN", "standard") == ("delhi", "dl", "in", "standard")
+    with pytest.raises(ValidationError):
+        ows.validate_units("Delhi", "DL", "IN", "random")
     
 
