c6a52dfdae49cedec73654a858d5958688471253
Nitesh <niteshsoniofficial@gmail.com>
2022-03-03 14:55:28 +0530

    remove some dependencies from requirements.txt

--- test_main.py
+++ test_main.py
@@ -1,10 +1,8 @@
 from services import openweather_service as ows
-import pytest
 from models.validation_error import ValidationError
 
 def test_validate_units():
     assert ows.validate_units("Delhi", "DL", "", "metric") == ("delhi", "dl", "us", "metric")
     assert ows.validate_units("Delhi", "DL", "IN", "standard") == ("delhi", "dl", "in", "standard")
-    with pytest.raise(ValidationError):
-        ows.validate_units("Delhi", "DL", "IN", "Default")
+    
 
